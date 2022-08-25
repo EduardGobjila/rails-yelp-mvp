b387e8c04ff7e23ec6d39a49dc92a23a4f335931
Eduard Gobjila <edikgobjila01@gmail.com>
2022-08-25 10:54:30 +0100

    need to work on it

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,17 @@ puts 'Cleaning database...'
 Restaurant.destroy_all
 
 puts 'Creating restaurants...'
-dishoom = {name: 'Dishoom', address: '7 Boundary St, London E2 7JE' ,phone_number: 02043434343, rating: 5}
-pizza_east = {name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ',phone_number: 0241432234, rating: 4}
-hoxton =  {name: 'Hoxton100', address: 'Muncesti 162 a', phone_number: 020323232, rating: 5}
-kfc =  {name: "KFC East", address: "15 st panctas court n29ae", phone_number: 23243232, rating: 1}
-andys =  {name: "Andys Pizza", address: "Stefan Cel Mare 345",phone_number: 020343434, rating: 3}
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', phone_number: '02043434343', category: 'italian'}
+pizza_east = { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', phone_number: '0241432234', category: 'chinese' }
+hoxton =  { name: 'Hoxton100', address: 'Muncesti 162 a', phone_number: '020323232', category: 'french' }
+kfc =  { name: 'KFC East', address: '15 st panctas court n29ae', phone_number: '23243232', category: 'italian' }
+andys =  { name: 'Andys Pizza', address: 'Stefan Cel Mare 34', phone_number: '020343434', category: 'chinese' }
 
 
 [dishoom, pizza_east, hoxton, kfc, andys].each do |attributes|
   restaurant = Restaurant.create!(attributes)
   puts "Created #{restaurant.name}"
+  Review.create(rating: 3, content: 'dfdsafgd', restaurant: restaurant)
+  # puts
 end
-puts "Finished!"
+puts 'Finished!'
